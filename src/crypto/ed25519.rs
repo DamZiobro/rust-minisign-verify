cab3d5f92b8569b7f535b6db39fb7e143c7d2011
Damian Ziobro <damian@xmementoit.com>
2022-06-22 14:49:57 +0100

    update

--- src/crypto/ed25519.rs
+++ src/crypto/ed25519.rs
@@ -50,12 +50,19 @@ pub fn verify(message: &[u8], public_key: &[u8], signature: &[u8]) -> bool {
     sc_reduce(&mut hash);
 
     let r = GeP2::double_scalarmult_vartime(hash.as_ref(), a, &signature[32..64]);
-    println!("verify -> return TRUE -> {:?}", r.to_bytes().as_ref());
-    let num_bytes = r.to_bytes()
+    println!("verify -> return TRUE");
+    println!("signature: {:?}", signature);
+    println!("r        : {:?}", r.as_bytes().as_ref());
+    println!("len(r): {:?}", r.to_bytes.as_ref().len());
+    println!("len(signature): {:?}", signature.len());
+    let zip_object = r.to_bytes()
         .as_ref()
         .iter()
-        .zip(signature.iter())
+        .zip(signature.iter());
+    println!("zip_object: {:?}", zip_object);
+    let num_bytes = zip_object
         .fold(0, |acc, (x, y)| acc | (x ^ y));
 
+    println!("num_bytes: {:?}", num_bytes);
     num_bytes == 0
 }
